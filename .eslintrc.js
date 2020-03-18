7bc05797c2f6996eb955e7bcb3264ab0fa9c735c
IuriVeloso <iuriv.felix@gmail.com>
2020-03-18 16:57:44 -0300

    ESLint&Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'prettier',
+    'prettier/react',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    "react", "jsx-a11y", "import", "react-hooks", "prettier"
+  ],
+  rules: {
+      "prettier/prettier": "error",
+      "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+      "react/jsx-indent": [2, 2]
+    },
+};
